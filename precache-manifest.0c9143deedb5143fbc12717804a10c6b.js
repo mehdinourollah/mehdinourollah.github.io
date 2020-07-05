c1243cb5e35bdd6e7e3417a6eda9948d6549c18c
meiki <mehdinourollah@gmail.com>
2020-07-05 17:39:25 +0430

    clean the mess

---
+++ precache-manifest.0c9143deedb5143fbc12717804a10c6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d42b6dd29a18b0920c9d5c6ce6c2223",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9079b1d848d76aca98e2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ae0b42b5f39be48a9eb2",
+    "url": "/static/js/2.69bbeedd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69bbeedd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9079b1d848d76aca98e2",
+    "url": "/static/js/main.b199dac2.chunk.js"
+  },
+  {
+    "revision": "4b041d7b71a4e598f53b",
+    "url": "/static/js/runtime-main.dbb89ece.js"
+  }
+]);
