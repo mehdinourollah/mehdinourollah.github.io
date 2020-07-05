18f6c152df54fbc78f7569f074103490093b219c
meiki <mehdinourollah@gmail.com>
2020-07-05 17:32:29 +0430

    reactify

---
+++ src/Layout/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Footer(props) {
+
+  return (
+    
+      <footer className="footer">
+        <div className="footer__inner">
+          <div className="footer__content">
+            <span>©
+                </span>
+            <span>Mehdi Nourollah</span>
+          </div>
+        </div>
+        <div className="footer__inner">
+          <div className="footer__content">
+          </div>
+        </div>
+        <div className="footer__inner">
+          <div className="footer__content">
+          </div>
+        </div>
+      </footer>
+
+    
+  );
+}
+
+export default Footer;
