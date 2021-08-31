2dbcf4bce7862354b9f7df0640da23ad9487f33b
Mehdi Nourollah <mehdi.nourollah@tegsoft.com>
2021-08-31 14:27:42 +0300

    minor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-
-import React from 'react';
-import Header from './Layout/Header'
-import Content from './Layout/Content'
-import Footer from './Layout/Footer'
-
-
-
+import React from "react";
+import Header from "./Layout/Header";
+import Content from "./Layout/Content";
+import Footer from "./Layout/Footer";
 
 function App() {
-  document.title = "Mehdi Nourollah"
+  let msg = "%c Hi 👋! Welcome to my site!";
+  let styles = [
+    "font-size: 12px",
+    "font-family: monospace",
+    "background: white",
+    "display: inline-block",
+    "color: black",
+    "padding: 8px 19px",
+    "border: 1px dashed;",
+  ].join(";");
+  console.log(msg, styles);
+  document.title = "Mehdi Nourollah";
   return (
     <>
       <Header />
