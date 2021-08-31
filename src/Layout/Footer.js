c4e087c288f2e31a0a2f816c4f72627ef9d06947
Mehdi Nourollah <mehdi.nourollah@tegsoft.com>
2021-08-31 14:37:23 +0300

    minor

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,63 +1,65 @@
-import React from 'react';
-import { Container, Row, Col, Table } from 'reactstrap';
-
+import React from "react";
+import { Container, Row, Col, Table } from "reactstrap";
 
 class Footer extends React.Component {
-  constructor(props) { // Init props and state
+  constructor(props) {
+    // Init props and state
     super(props);
     this.state = { quote: null };
-
   }
 
   componentDidMount() {
     // Simple POST request with a JSON body using fetch
 
-
-
-    fetch('https://quotes.rest/qod.json')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        try { this.setState({ quote: data.contents.quotes[0] }) } catch{ }
-
-      })
-
+    fetch("https://quotes.rest/qod.json")
+      .then((response) => response.json())
+      .then((data) => {
+        try {
+          this.setState({ quote: data.contents.quotes[0] });
+        } catch {}
+      });
   }
   render(props) {
-    const { quote } = this.state
+    const { quote } = this.state;
     const divStyle = {
-      color: 'blue',
+      color: "blue",
     };
     return (
       <div>
-
-
-
-
-
-
-
         <footer className="footer">
-
           <div className="footer__inner">
+            <div className="footer__content">
+              {quote ? (
+                <Container fluid={true}>
+                  <Row>
+                    <Col xs="3">
+                      <span style={{ position: "absolute", bottom: 0 }}>
+                        {new Date().getFullYear()}{" "}
+                      </span>
+                    </Col>
+                    <Col xs="6">
+                      <div style={{ fontSize: "auto" }}>
+                        {" "}
+                        {quote ? quote.quote : ""} <br />{" "}
+                        {quote ? quote.author : ""}
+                      </div>
+                    </Col>
 
-
-          <div className="footer__content">
-          {quote ?
-            <Container fluid={true}>
-              <Row>
-                <Col xs="3"><span style={{ position: 'absolute', bottom: 0 }}>{new Date().getFullYear()}               </span></Col>
-                <Col xs="6" ><div style={{ fontSize: 'auto' }} > {quote ? quote.quote : ''} <br /> {quote ? quote.author : ''}</div></Col>
-
-
-
-                <Col xs="3"><span style={{ position: 'absolute', bottom: 0 }}>Mehdi Nourollah</span></Col>
-              </Row>
-            </Container>
-            : <> <span>{new Date().getFullYear()}               </span>
-              <span>Mehdi Nourollah</span>
-            </>}
-          </div>
+                    <Col xs="3">
+                      <span style={{ position: "absolute", bottom: 0 }}>
+                        Mehdi Nourollah
+                      </span>
+                    </Col>
+                  </Row>
+                </Container>
+              ) : (
+                <>
+                  {" "}
+                  <span>{new Date().getFullYear()} </span>
+                  <span>Mehdi Nourollah</span>
+                </>
+              )}
+            </div>
           </div>
           {/* <div className="footer__inner">
             <div className="footer__content">
@@ -68,9 +70,7 @@ class Footer extends React.Component {
             </div>
           </div> */}
         </footer>
-
       </div>
-
     );
   }
 }
